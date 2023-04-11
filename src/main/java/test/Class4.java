d0e18081f002b9183103e026612f83f98ea6a614
ahmddhelal <96657191+ahmddhelal@users.noreply.github.com>
2023-04-11 12:26:39 +0200

    commit#8

---
+++ src/main/java/test/Class4.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Class4 {
+    @Test
+    public void Class4test1() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void Class4test2() {
+        Assert.assertTrue(true);
+    }
+
+    @Test()
+    public void Class4est3() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void Class4test4() {
+        Assert.assertTrue(false);
+        System.out.println(":::::::: Failed Class4test4 ::::::::");
+    }
+}
