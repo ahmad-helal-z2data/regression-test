aa72927d1cdbdc20e2cb62191f6d481f187f395c
ahmddhelal <96657191+ahmddhelal@users.noreply.github.com>
2023-04-12 11:21:24 +0200

    added class #6

---
+++ src/main/java/test/Class6.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Class6 {
+    @Test
+    public void Class6test1() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void Class6test2() {
+        Assert.assertTrue(true);
+    }
+
+    @Test()
+    public void Class6est3() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void Class6test4() {
+        Assert.assertTrue(false);
+        System.out.println(":::::::: Failed Class5test4 ::::::::");
+    }
+}
